c4263558e36b88d6cf75b6317275c15c142603b1
Vinicius-E <viniciuseduardo0702@hotmail.com>
2021-11-05 12:33:31 -0300

    Project Created Version 1.0

---
+++ app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Page401Component } from './page401/page401.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    Page401Component,
+    RegistrationComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
